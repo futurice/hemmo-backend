69751a6bd186d37aa2353d6ce1ecd07493750536
Oskar Vuola <oskar.vuola@futurice.com>
2016-07-26 16:39:15 +0300

    Add register function

--- db/migrations/000_initial.js
+++ db/migrations/000_initial.js
@@ -5,7 +5,7 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('users', function(table) {
       table.increments('id').primary();
-      table.string('username').notNullable();
+      table.string('name').notNullable();
       table.string('token').unique().notNullable();
     })
 
--- src/server.js
+++ src/server.js
@@ -16,30 +16,36 @@ server.connection({
   }
 });
 
-// Add the route
-server.route({
-  method: 'GET',
-  path: '/adduser',
-  handler: function (request, reply) {
-    knex('users').insert({
-      username: 'Pekka',
-      token: 'akkajfksjdfkdfkjdf'
-    })
-    .then(function(shit) {
-      console.log(shit);
-    });
-    return reply('Added user');
-  }
-});
-
 server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-
     var users = knex.select('name').from('users')
     .then(function(rows) {
       return reply(rows);
+    });
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/register',
+  handler: function (request, reply) {
+    var name = request.payload['name'];
+
+    require('crypto').randomBytes(48, function(err, buffer) {
+      var token = buffer.toString('hex');
+
+      knex('users').insert({
+        name: name,
+        token: token
+      })
+      .then(function(res) {
+        console.log(res);
+        return reply({
+          token: token
+        });
+      });
     });
   }
 });
