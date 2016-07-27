3e3366aa67c0d14998bb7df6a299fd4c6a16a1a8
Rasmus Eskola <fruitiex@gmail.com>
2016-07-27 13:13:00 +0300

    Move routes into routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,86 @@
 /*eslint-disable object-curly-spacing*/
+
+import knex from './db'
+import Boom from 'boom';
+import Promise from 'bluebird';
+
+var randomBytes = Promise.promisify(require("crypto").randomBytes);
+
+let routes = [];
+
+routes.push({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    var users = knex.select('name').from('users')
+    .then(function(rows) {
+      return reply(rows);
+    });
+  }
+});
+
+routes.push({
+  method: 'POST',
+  path: '/session',
+  handler: function(request, reply) {
+    var token = request.headers['token'];
+    var sessionDictionary = {};
+
+    knex.select('id').from('users').where('token', token)
+    .then(function(rows) {
+      var user_id = rows[0].id;
+      var timestamp = knex.fn.now();
+
+      sessionDictionary['user_id'] = user_id;
+      sessionDictionary['started_at'] = timestamp;
+
+      return randomBytes(48);
+    })
+    .then(function(bytes) {
+      var session_id = bytes.toString('hex');
+      sessionDictionary['session_id'] = session_id;
+
+      return knex('sessions').insert(sessionDictionary);
+    })
+    .then(function(res) {
+      return reply({
+        session_id: sessionDictionary['session_id']
+      });
+    })
+    .catch(function(err) {
+      return reply(Boom.badRequest(err));
+    });
+  }
+});
+
+routes.push({
+  method: 'POST',
+  path: '/register',
+  handler: function (request, reply) {
+    var name = request.payload['name'];
+    var userDictionary = {};
+    randomBytes(48)
+    .then(function(bytes) {
+      var token = bytes.toString('hex');
+      if (name === undefined || name.length === 0) {
+        throw new Error('Invalid name');
+      }
+
+      userDictionary = {
+        name: name,
+        token: token
+      };
+      return knex('users').insert(userDictionary);
+    })
+    .then(function(res) {
+      return reply({
+        token: userDictionary['token']
+      });
+    })
+    .catch(function(err) {
+      return reply(Boom.badRequest(err));
+    });
+  }
+});
+
+export default routes;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,9 @@
 import Hapi from 'hapi';
-import Boom from 'boom';
-import Promise from 'bluebird';
-//import routes from './routes';
+import routes from './routes';
 import config from './config';
-import knex from './db'
 
 
 process.env.TZ = 'UTC';
-
-//var Promise = require('bluebird');
-var randomBytes = Promise.promisify(require("crypto").randomBytes);
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -22,82 +16,7 @@ server.connection({
   }
 });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    var users = knex.select('name').from('users')
-    .then(function(rows) {
-      return reply(rows);
-    });
-  }
-});
-
-server.route({
-  method: 'POST',
-  path: '/session',
-  handler: function(request, reply) {
-    var token = request.headers['token'];
-    var sessionDictionary = {};
-
-    knex.select('id').from('users').where('token', token)
-    .then(function(rows) {
-      var user_id = rows[0].id;
-      var timestamp = knex.fn.now();
-
-      sessionDictionary['user_id'] = user_id;
-      sessionDictionary['started_at'] = timestamp;
-
-      return randomBytes(48);
-    })
-    .then(function(bytes) {
-      var session_id = bytes.toString('hex');
-      sessionDictionary['session_id'] = session_id;
-
-      return knex('sessions').insert(sessionDictionary);
-    })
-    .then(function(res) {
-      return reply({
-        session_id: sessionDictionary['session_id']
-      });
-    })
-    .catch(function(err) {
-      return reply(Boom.badRequest(err));
-    });
-  }
-});
-
-server.route({
-  method: 'POST',
-  path: '/register',
-  handler: function (request, reply) {
-    var name = request.payload['name'];
-    var userDictionary = {};
-    randomBytes(48)
-    .then(function(bytes) {
-      var token = bytes.toString('hex');
-      if (name === undefined || name.length === 0) {
-        throw new Error('Invalid name');
-      }
-
-      userDictionary = {
-        name: name,
-        token: token
-      };
-      return knex('users').insert(userDictionary);
-    })
-    .then(function(res) {
-      return reply({
-        token: userDictionary['token']
-      });
-    })
-    .catch(function(err) {
-      return reply(Boom.badRequest(err));
-    });
-  }
-});
-
-//server.route(routes);
+server.route(routes);
 
 // Start the server
 server.start((err) => {
