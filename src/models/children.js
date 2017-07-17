7f4a0563b26114e5064f1da158274053211bf802
Sara Sytelä <sara.sytela@gmail.com>
2017-07-17 10:49:01 +0300

    Changed a bit query for children (#15)
    * Changed a bit query for children
    
    * Fixed broken merge
    
    * Fixed possible sql injection
    
    * Improved if clause
    
    * Further improvements

--- src/models/children.js
+++ src/models/children.js
@@ -1,35 +1,47 @@
 import uuid from 'uuid/v4';
 import knex, { likeFilter, exactFilter } from '../utils/db';
 
-export const dbGetChildren = filters =>
-  knex('children')
+
+export const dbGetChildren = filters => {
+  let threeMonthsAgo = new Date();
+  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
+  threeMonthsAgo = Math.round(threeMonthsAgo.getTime() / 1000);
+
+  let query = knex('children')
     .select([
       'children.*',
       'employees.name as assigneeName',
-      'prevFeedback.prevFeedbackDate',
+      'feedback.created as lastFeedbackDate',
+      knex.raw(`case when feedback.created < to_timestamp(?) then 1 else 0 end as alert`, threeMonthsAgo),
     ])
-    .where(
-      likeFilter({
-        'children.name': filters.name,
-        'employees.name': filters.assigneeName,
-      }),
-    )
-    .andWhere(
-      exactFilter({
-        assigneeId: filters.assigneeId,
-      }),
-    )
+    .where(likeFilter({
+      'children.name': filters.name,
+      'employees.name': filters.assigneeName,
+    }))
+    .andWhere(exactFilter({
+      assigneeId: filters.assigneeId
+    }))
     .leftOuterJoin('employees', 'children.assigneeId', 'employees.id')
     // Previous feedback
     .leftOuterJoin(
-      knex('feedback')
-        .select(['createdAt as prevFeedbackDate', 'childId'])
-        .orderBy('createdAt', 'desc')
-        .as('prevFeedback'),
+      knex('feedback').select([
+        'createdAt as created',
+        'childId',
+      ])
+      .orderBy('createdAt', 'desc')
+      .as('feedback'),
+
       'children.id',
-      'prevFeedback.childId',
+      'feedback.childId',
     )
-    .orderBy(filters.orderBy || 'children.name', filters.order);
+    .orderBy(filters.orderBy || 'children.name', filters.order)
+
+    if (filters && filters.alert === 1) {
+      query.whereRaw(`feedback.created < to_timestamp(?)`, threeMonthsAgo);
+    }
+
+    return query;
+};
 
 export const dbGetChild = id =>
   knex('children')
