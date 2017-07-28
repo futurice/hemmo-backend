531837e2fb8d6221751a7d8d94bbbb8b1d720cfc
Rasmus Eskola <fruitiex@gmail.com>
2017-07-28 16:22:04 +0300

    Try fixing text attachments

--- src/handlers/attachments.js
+++ src/handlers/attachments.js
@@ -10,14 +10,24 @@ export const getAttachment = (request, reply) =>
 export const createAttachment = (request, reply) => {
   const bufs = [];
 
-  request.payload.data.on('data', chunk => bufs.push(chunk));
-  request.payload.data.on('end', () =>
+  if (typeof request.payload.data === 'string') {
     dbCreateAttachment(
       request.params.feedbackId,
-      Buffer.concat(bufs),
-      request.payload.data.hapi.headers['content-type'],
+      request.payload.data,
+      'application/text',
     )
       .then(reply)
-      .catch(err => reply(Boom.badImplementation(err))),
-  );
+      .catch(err => reply(Boom.badImplementation(err)));
+  } else {
+    request.payload.data.on('data', chunk => bufs.push(chunk));
+    request.payload.data.on('end', () =>
+      dbCreateAttachment(
+        request.params.feedbackId,
+        Buffer.concat(bufs),
+        request.payload.data.hapi.headers['content-type'],
+      )
+        .then(reply)
+        .catch(err => reply(Boom.badImplementation(err))),
+    );
+  }
 };
